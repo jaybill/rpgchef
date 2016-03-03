d59c103342a1403f60f4043dd371a60170a15102
Jaybill McCarthy <heybill@jaybill.com>
2016-03-03 15:56:05 -0800

    Added monsters model

---
+++ src/server/models/monsters.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import log from 'loglevel';
+import Sequelize from 'sequelize';
+
+export default function NewMonster(DbConn) {
+  const Monsters = DbConn.define('weapons',
+    {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      "xp": Sequelize.BIGINT,
+      "CHA": Sequelize.BIGINT,
+      "CON": Sequelize.BIGINT,
+      "DEX": Sequelize.BIGINT,
+      "INT": Sequelize.BIGINT,
+      "STR": Sequelize.BIGINT,
+      "WIS": Sequelize.BIGINT,
+      "size": Sequelize.STRING,
+      "speed": Sequelize.STRING,
+      "senses": Sequelize.STRING,
+      "skills": Sequelize.STRING,
+      "traits": Sequelize.JSONB,
+      "actions": Sequelize.JSONB,
+      "alignment": Sequelize.STRING,
+      "challenge": Sequelize.STRING,
+      "hitpoints": Sequelize.STRING,
+      "languages": Sequelize.STRING,
+      "armorclass": Sequelize.STRING,
+      "raceOrType": Sequelize.STRING,
+      "savingThrows": Sequelize.STRING,
+      "legendaryPoints": Sequelize.BIGINT,
+      "damageImmunities": Sequelize.STRING,
+      "damageResistances": Sequelize.STRING,
+      "legendaryActions": Sequelize.JSONB,
+      "conditionImmunities": Sequelize.STRING,
+      "damageVulnerabilities": Sequelize.STRING
+    }, {
+      timestamps: false
+    }
+  );
+  return Monsters;
+}
